8290d10f72e4b3fcb21aca2ca8a5cb68839add02
Jrocha2381 <juanrocro@unisabana.edu.co>
2026-02-17 11:50:11 -0500

    Datos productos
    Agrego datos de los productos, aun faltan imagenes

---
+++ js/data.js
@@ -0,0 +1,98 @@
+export const productos = [
+  {
+    id: 1,
+    nombre: "Cuaderno Profesional",
+    categoria: "Escolar",
+    precio: 12000,
+    imagen: "cuaderno.jpg",
+    descripcion: "Cuaderno argollado 100 hojas"
+  },
+  {
+    id: 2,
+    nombre: "Lapicero Azul",
+    categoria: "Escritura",
+    precio: 2000,
+    imagen: "lapiceroazul.jpg",
+    descripcion: "Tinta azul punta fina"
+  },
+  {
+    id: 3,
+    nombre: "Resaltador Amarillo",
+    categoria: "Escritura",
+    precio: 3500,
+    imagen: "resaltador-amarillo.webp",
+    descripcion: "Color fluorescente"
+  },
+  {
+    id: 4,
+    nombre: "Borrador Blanco",
+    categoria: "Escolar",
+    precio: 1500,
+    imagen: "borrador.webp",
+    descripcion: "Borrador suave para lápiz"
+  },
+  {
+    id: 5,
+    nombre: "Regla 30cm",
+    categoria: "Escolar",
+    precio: 2500,
+    imagen: "img/regla.jpg",
+    descripcion: "Regla plástica transparente"
+  },
+  {
+    id: 6,
+    nombre: "Carpeta Plástica",
+    categoria: "Oficina",
+    precio: 5000,
+    imagen: "img/carpeta.jpg",
+    descripcion: "Carpeta con broche"
+  },
+  {
+    id: 7,
+    nombre: "Marcador Permanente",
+    categoria: "Escritura",
+    precio: 4000,
+    imagen: "img/marcador.jpg",
+    descripcion: "Color negro punta gruesa"
+  },
+  {
+    id: 8,
+    nombre: "Tijeras Escolares",
+    categoria: "Escolar",
+    precio: 6000,
+    imagen: "img/tijeras.jpg",
+    descripcion: "Tijeras punta redonda"
+  },
+  {
+    id: 9,
+    nombre: "Pegante en Barra",
+    categoria: "Escolar",
+    precio: 3000,
+    imagen: "img/pegante.jpg",
+    descripcion: "Pegante sólido 20g"
+  },
+  {
+    id: 10,
+    nombre: "Calculadora Básica",
+    categoria: "Electrónica",
+    precio: 25000,
+    imagen: "calculadora-basica.jpg",
+    descripcion: "Calculadora de 8 dígitos"
+  },
+  {
+    id: 11,
+    nombre: "Agenda 2026",
+    categoria: "Oficina",
+    precio: 18000,
+    imagen: "img/agenda.jpg",
+    descripcion: "Agenda diaria tapa dura"
+  },
+  {
+    id: 12,
+    nombre: "Cartuchera",
+    categoria: "Escolar",
+    precio: 15000,
+    imagen: "img/cartuchera.jpg",
+    descripcion: "Cartuchera con cierre"
+  }
+];
