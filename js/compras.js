31fc00fe7ae5f20e97ee0c890cec2f6f4cbcee9e
jero-crack1 <jeronimorupr@unisabana.edu.co>
2026-05-22 07:34:18 -0500

    Revert "dd"
    This reverts commit 6e9b65fbcda4dcebe9bba639c2ff14ac4cf9e087.

--- js/compras.js
+++ js/compras.js
@@ -40,8 +40,6 @@ async function registrarCompra(items, proveedorId = null) {
 
   if (backendDisponibleCompras()) {
     compraCreada = await window.Backend.post("compras", payload);
-    await window.sincronizarProductosBackend();
-    return compraCreada;
   } else {
     const comprasPrevias = JSON.parse(localStorage.getItem("compras") || "[]");
     compraCreada.id = Date.now();
